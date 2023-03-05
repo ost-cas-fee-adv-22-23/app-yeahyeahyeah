aa1af19648eeda203575c88e4f74926376b2f947
webrooster <roland.vonaesch@fhnw.ch>
2023-03-05 14:17:43 +0100

    fix(MumbleHeader): profile image, rm page

--- src/components/mumble/MumbleHeader.tsx
+++ src/components/mumble/MumbleHeader.tsx
@@ -17,7 +17,11 @@ export const MumbleHeader: React.FC<MumbleHeaderProps> = ({ alias }) => {
   return (
     <>
       <div tw="flex flex-row justify-end items-end z-0 w-full relative top-0 mt-0">
-        <ImageContainer type="banner-view" alt="image alt tag" src="https://placebeard.it/640x360" />
+        <ImageContainer
+          alt="This is a profile picture"
+          onImageIconClick={function noRefCheck() {}}
+          src="https://picsum.photos/640/360"
+        />
       </div>
       <div tw="flex flex-row justify-end z-10 relative right-32 -top-108 h-0">
         <Avatar variant="xlarge" src="https://media.giphy.com/media/cfuL5gqFDreXxkWQ4o/giphy.gif" alt="Username" />
@@ -52,7 +56,9 @@ export const MumbleHeader: React.FC<MumbleHeaderProps> = ({ alias }) => {
           linkComponent={Link}
         />
       </div>
+
       <Paragraph text="Schreib was über dich!" color="default" mbSpacing="32" />
+
       <Switch
         fCallBack={(value) => console.log(value)}
         options={[
