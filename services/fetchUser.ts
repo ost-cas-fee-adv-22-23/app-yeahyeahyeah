8fed8935f75114eac5bf3fe2b5292a2a303d2684
Thomas Schallert <thomas.schallert@fhnw.ch>
2023-03-08 23:19:47 +0100

    fix: refactor fetchUser service

--- services/fetchUser.ts
+++ services/fetchUser.ts
@@ -1,15 +1,9 @@
 import axios from 'axios';
 import { User } from './qwacker';
 
-export const fetchUser = async (params?: {
-  id?: string;
-
-  token: string;
-}) => {
+export const fetchUser = async (params?: { id: string; token: string }) => {
   const { id, token } = params || {};
-
   const idParams = id ? `${id}` : '';
-
   const url = `${process.env.NEXT_PUBLIC_QWACKER_API_URL}/users/${idParams}`;
 
   const { data } = await axios.get(url, {
