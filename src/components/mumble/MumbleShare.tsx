a3167cde4335267a0902d48854658eff63a8c5cb
webrooster <roland.vonaesch@fhnw.ch>
2023-03-18 13:11:26 +0100

    fix(Sharelink, MumbleDetail): styles, cleanups

--- src/components/mumble/MumbleShare.tsx
+++ src/components/mumble/MumbleShare.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import process from 'process';
 import { ShareButton } from '@smartive-education/design-system-component-library-yeahyeahyeah';
 
 type MumbleShareProps = {
-  path?: string;
+  id: string;
 };
 
-export const MumbleShare = ({ id, path }: MumbleShareProps) => {
+export const MumbleShare = ({ id }: MumbleShareProps) => {
+  const host = process.env.NEXT_PUBLIC_URL;
+  const directory = 'mumble';
+
   const handleShareLink = async () => {
     if ('clipboard' in navigator) {
-      return await navigator.clipboard.writeText(`${path}`);
+      return await navigator.clipboard.writeText(`${host}/${directory}/${id}`);
     }
   };
   return <ShareButton label="Copy link" onClick={handleShareLink} />;
--- src/pages/mumble/[id].tsx
+++ src/pages/mumble/[id].tsx
@@ -89,9 +89,3 @@ export const getServerSideProps: GetServerSideProps = async ({ req, query: { id 
     },
   };
 };
-
-export const ArticleMumble = tw.article`flex flex-col justify-start items-start w-full bg-slate-white py-32 pt-16 px-16 sm:px-48 rounded-lg mb-16`;
-export const ArticleHeader = tw.div`flex flex-row items-center gap-16 relative left-0 sm:-left-[86px] mb-32`;
-export const ArticleHeaderContent = tw.div`flex flex-col`;
-export const ArticleDatas = tw.div`flex flex-row gap-16`;
-export const ArticleInteraction = tw.div`flex flex-row`;
