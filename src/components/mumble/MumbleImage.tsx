4cab2dd286867fb2604475142cb69ea0f88c376b
webrooster <roland.vonaesch@fhnw.ch>
2023-03-26 13:04:29 +0200

    feat(ImageModal): new feature

--- src/components/mumble/MumbleImage.tsx
+++ src/components/mumble/MumbleImage.tsx
@@ -1,6 +1,7 @@
+import React, { useState } from 'react';
 import Image from 'next/legacy/image';
 import { imageLoader } from '@/utils';
-import { ImageContainer } from '@smartive-education/design-system-component-library-yeahyeahyeah';
+import { ImageContainer, Modal } from '@smartive-education/design-system-component-library-yeahyeahyeah';
 
 type MumbleImageProps = {
   mediaUrl: string;
@@ -10,19 +11,52 @@ type MumbleImageProps = {
 };
 
 export const MumbleImage = ({ mediaUrl, text, width, height }: MumbleImageProps) => {
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  const handleClose = () => {
+    setOpen(!open);
+  };
+
   return (
-    <ImageContainer
-      type="container"
-      loader={imageLoader}
-      src={mediaUrl}
-      alt={text}
-      width={width}
-      height={height}
-      objectFit="cover"
-      loading="lazy"
-      placeholder="empty"
-      onImageIconClick={() => console.log('ImageContainer clicked')}
-      imageComponent={Image}
-    />
+    <>
+      {open === false && (
+        <ImageContainer
+          type="container"
+          loader={imageLoader}
+          src={mediaUrl}
+          alt={text}
+          width={width}
+          height={height}
+          objectFit="cover"
+          loading="lazy"
+          placeholder="empty"
+          onImageIconClick={() => handleClick()}
+          imageComponent={Image}
+        />
+      )}
+      {open === true && (
+        <Modal label="Mumble Image" isOpen={open} onClose={handleClose} wide={true}>
+          <Image
+            loader={imageLoader}
+            src={mediaUrl}
+            alt={text}
+            width="1280"
+            height="720"
+            layout="responsive"
+            objectFit="contain"
+            loading="lazy"
+            placeholder="empty"
+            sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 100vw,
+              100vw"
+            className="w-100 h-100"
+          />
+        </Modal>
+      )}
+    </>
   );
 };
