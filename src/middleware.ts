239898716f19652a5b5c6935df4a09b3d216e980
webrooster <roland.vonaesch@fhnw.ch>
2023-03-30 13:13:05 +0200

    fix(Middleware): remove logs

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,20 +3,19 @@ import { NextRequest, NextResponse } from 'next/server';
 export function middleware(request: NextRequest) {
   const { cookies } = request;
   const jwt = cookies.has('next-auth.session-token') || cookies.has('__Secure-next-auth.session-token');
-  console.log('request', request);
-  console.log('jwt', jwt);
+
   if (request.nextUrl.pathname.startsWith('/profile')) {
-    console.log('profile');
     if (!jwt) {
       return NextResponse.redirect(new URL('/landingpage', request.url));
     }
   }
+
   if (request.nextUrl.pathname.startsWith('/landingpage')) {
-    console.log('landingpage');
     if (jwt) {
       return NextResponse.redirect(new URL('/', request.url));
     }
   }
+
   return NextResponse.next();
 }
 
