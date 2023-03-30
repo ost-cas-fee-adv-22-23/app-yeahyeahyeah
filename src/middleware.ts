be6f0078900f02d64a9f12c8db2eb54e88cf2035
Thomas Schallert <thomas.schallert@fhnw.ch>
2023-03-30 10:47:19 +0200

    fix: preparing middleware for testing on live server

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,16 +3,23 @@ import { NextRequest, NextResponse } from 'next/server';
 export function middleware(request: NextRequest) {
   const { cookies } = request;
   const jwt = cookies.get('next-auth.session-token');
-
+  console.log('request', request);
+  console.log('jwt', jwt);
   if (request.nextUrl.pathname.startsWith('/profile')) {
+    console.log('profile');
     if (!jwt) {
       return NextResponse.redirect(new URL('/landingpage', request.url));
     }
   }
-
   if (request.nextUrl.pathname.startsWith('/landingpage')) {
+    console.log('landingpage');
     if (jwt) {
       return NextResponse.redirect(new URL('/', request.url));
     }
   }
+  return NextResponse.next();
 }
+
+export const config = {
+  matcher: ['/profile/:path*', '/landingpage/:path*'],
+};
--- src/pages/profile/[id].tsx
+++ src/pages/profile/[id].tsx
@@ -30,6 +30,8 @@ export default function Page({ creator, limit, fallbackUser, fallBackMyMumbles }
   const ref = useRef(null);
   const [isOnScreen] = useOnScreen(ref);
 
+  console.log('session', session);
+
   const { data: likes } = useSWR({ url: '/api/myLikes', token: session?.accessToken }, fetchMyLikes, {
     refreshInterval: 10000,
     revalidateOnFocus: false,
