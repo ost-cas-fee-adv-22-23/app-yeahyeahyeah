d04f95f3d7d69cb207b88ffcc33df508668a80f0
Thomas Schallert <thomas.schallert@fhnw.ch>
2023-04-01 18:32:24 +0200

    fix: tailwind-config - show colors in vscode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  presets: [require('@smartive-education/design-system-component-library-yeahyeahyeah')],
-  content: [
-    './src/**/*.{ts,tsx}',
-    './node_modules/@smartive-education/design-system-component-library-yeahyeahyeah/**/*.{js,ts,jsx,tsx}',
-  ],
+  presets: [require('@smartive-education/design-system-component-library-yeahyeahyeah/tailwind.config.js')],
   plugins: [],
 };
