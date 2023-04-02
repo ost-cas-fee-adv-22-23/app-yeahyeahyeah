626612fce8938a80601e752353ba74733bd28fd2
webrooster <roland.vonaesch@fhnw.ch>
2023-04-02 13:39:24 +0200

    fix(LoadingSpinner): default props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -131,7 +131,7 @@ export default function Page({ limit, fallback }: { limit: number; fallback: { '
             ));
           })}
         <div key="last" tw="invisible" ref={ref} />
-        {(isLoading || isValidating) && <LoadingSpinner />}
+        <div tw="h-64">{(isLoading || isValidating) && <LoadingSpinner />}</div>
       </Container>
     </>
   );
