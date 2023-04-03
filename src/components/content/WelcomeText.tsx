e9e581c844446a1b7b906ee0c9c51a302c41f501
webrooster <roland.vonaesch@fhnw.ch>
2023-04-03 20:04:03 +0200

    feat(WelcomeText): correct sematic html

--- src/components/content/WelcomeText.tsx
+++ src/components/content/WelcomeText.tsx
@@ -1,16 +1,13 @@
-import { Heading } from '@smartive-education/design-system-component-library-yeahyeahyeah';
+import { Heading, Paragraph } from '@smartive-education/design-system-component-library-yeahyeahyeah';
 
 export const WelcomeText: React.FC = () => {
   return (
     <div tw="mb-16">
       <Heading label="Willkommen auf Mumble" color="violet" tag="h2" size="default" mbSpacing="8" />
-      <Heading
-        label="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna."
-        color="light"
-        tag="h4"
-        size="default"
-        mbSpacing="32"
-      />
+      <Paragraph color="light" size="large" mbSpacing="32">
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
+        magna.
+      </Paragraph>
     </div>
   );
 };
