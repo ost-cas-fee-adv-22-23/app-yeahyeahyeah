b09c3f8cbca4c03313557d63254298ac2f5e45de
webrooster <roland.vonaesch@fhnw.ch>
2023-04-12 15:59:52 +0200

    fix(WelcomeText): semantic correct html

--- src/components/content/WelcomeText.tsx
+++ src/components/content/WelcomeText.tsx
@@ -1,13 +1,16 @@
-import { Heading, Paragraph } from '@smartive-education/design-system-component-library-yeahyeahyeah';
+import tw from 'twin.macro';
+import { Heading } from '@smartive-education/design-system-component-library-yeahyeahyeah';
 
 export const WelcomeText: React.FC = () => {
   return (
-    <div tw="mb-16">
-      <Heading label="Willkommen auf Mumble" color="violet" tag="h2" size="default" mbSpacing="8" />
-      <Paragraph color="light" size="large" mbSpacing="32">
+    <div tw="mb-16 px-8">
+      <Heading label="Willkommen auf Mumble" color="violet" tag="h1" size="default" mbSpacing="8" />
+      <ParagraphWrapper>
         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
         magna.
-      </Paragraph>
+      </ParagraphWrapper>
     </div>
   );
 };
+
+const ParagraphWrapper = tw.p`text-lg font-semibold text-slate-500 leading-relaxed`;
