ec2d195a33a30adf3a351e031d537d3851b77483
Thomas Schallert <thomas.schallert@fhnw.ch>
2023-04-18 17:04:03 +0200

    feat: use spread syntax for reducer

--- src/reducer/textbox-reducer.ts
+++ src/reducer/textbox-reducer.ts
@@ -17,91 +17,61 @@ export function reducer(state: TextBoxState, action: TextBoxAction): TextBoxStat
   switch (action.type) {
     case 'SET_ERROR_MESSAGE': {
       return {
-        fileUploadError: state.fileUploadError,
+        ...state,
         errorMessage: action.payload as string,
-        file: state.file,
-        showModal: state.showModal,
-        inputValue: state.inputValue,
       };
     }
     case 'CLEAR_ERROR_MESSAGE': {
       return {
-        fileUploadError: state.fileUploadError,
+        ...state,
         errorMessage: '',
-        file: state.file,
-        showModal: state.showModal,
-        inputValue: state.inputValue,
       };
     }
     case 'SET_FILE_UPLOAD_ERROR': {
       return {
-        errorMessage: state.errorMessage,
+        ...state,
         fileUploadError: action.payload as string,
-        file: state.file,
-        showModal: state.showModal,
-        inputValue: state.inputValue,
       };
     }
     case 'CLEAR_FILE_UPLOAD_ERROR': {
       return {
-        errorMessage: state.errorMessage,
+        ...state,
         fileUploadError: '',
-        file: state.file,
-        showModal: state.showModal,
-        inputValue: state.inputValue,
       };
     }
     case 'SET_FILE': {
       return {
-        errorMessage: state.errorMessage,
-        fileUploadError: state.fileUploadError,
+        ...state,
         file: action.payload as UploadImage,
-        showModal: state.showModal,
-        inputValue: state.inputValue,
       };
     }
     case 'CLEAR_FILE': {
       return {
-        errorMessage: state.errorMessage,
-        fileUploadError: state.fileUploadError,
+        ...state,
         file: null,
-        showModal: state.showModal,
-        inputValue: state.inputValue,
       };
     }
     case 'SHOW_MODAL': {
       return {
-        errorMessage: state.errorMessage,
-        fileUploadError: state.fileUploadError,
-        file: state.file,
+        ...state,
         showModal: true,
-        inputValue: state.inputValue,
       };
     }
     case 'CLOSE_MODAL': {
       return {
-        errorMessage: state.errorMessage,
-        fileUploadError: state.fileUploadError,
-        file: state.file,
+        ...state,
         showModal: false,
-        inputValue: state.inputValue,
       };
     }
     case 'SET_INPUT_VALUE': {
       return {
-        errorMessage: state.errorMessage,
-        fileUploadError: state.fileUploadError,
-        file: state.file,
-        showModal: state.showModal,
+        ...state,
         inputValue: action.payload as string,
       };
     }
     case 'CLEAR_INPUT_VALUE': {
       return {
-        errorMessage: state.errorMessage,
-        fileUploadError: state.fileUploadError,
-        file: state.file,
-        showModal: state.showModal,
+        ...state,
         inputValue: '',
       };
     }
