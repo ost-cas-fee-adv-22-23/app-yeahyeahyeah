13b9d824de065e0d31059e4568d9c705c3612a60
Thomas Schallert <thomas.schallert@fhnw.ch>
2023-05-15 18:45:09 +0200

    fix: config

--- __e2e__/auth.setup.ts
+++ __e2e__/auth.setup.ts
@@ -2,7 +2,12 @@ import { test as setup } from '@playwright/test';
 import * as dotenv from 'dotenv';
 const config = dotenv.config();
 
+const user = process.env.ZITADEL_USER || '';
+const pw = process.env.ZITADEL_PW || '';
+
 console.log('dotenv config', config.parsed);
+console.log('user', user);
+console.log('pw', pw);
 
 setup('authenticate', async ({ page }) => {
   const authFile = './playwright/.auth/user.json';
@@ -13,9 +18,9 @@ setup('authenticate', async ({ page }) => {
   await page.goto('http://localhost:3000');
   await page.getByRole('button', { name: 'Login' }).click();
   await page.getByPlaceholder('username@domain').click();
-  await page.getByPlaceholder('username@domain').fill(process.env.ZITADEL_USER || '');
+  await page.getByPlaceholder('username@domain').fill(user);
   await page.getByRole('button', { name: 'next' }).click();
-  await page.getByLabel('Password').fill(process.env.ZITADEL_PW || '');
+  await page.getByLabel('Password').fill(pw);
   await page.getByRole('button', { name: 'next' }).click();
   await page.context().storageState({ path: authFile });
 });
