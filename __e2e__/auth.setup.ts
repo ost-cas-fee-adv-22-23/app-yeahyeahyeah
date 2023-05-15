de98b65d55452cadc3c63734ecdcb47d5b768dd6
Thomas Schallert <thomas.schallert@fhnw.ch>
2023-05-15 18:49:51 +0200

    fix: config

--- __e2e__/auth.setup.ts
+++ __e2e__/auth.setup.ts
@@ -13,14 +13,13 @@ setup('authenticate', async ({ page }) => {
   const authFile = './playwright/.auth/user.json';
 
   console.log('page', page.getByRole('button', { name: 'Login' }));
-  console.log('page', page.getByLabel(''));
 
   await page.goto('http://localhost:3000');
   await page.getByRole('button', { name: 'Login' }).click();
   await page.getByPlaceholder('username@domain').click();
   await page.getByPlaceholder('username@domain').fill(user);
-  await page.getByRole('button', { name: 'next' }).click();
+  await page.locator('#submit-button').click();
   await page.getByLabel('Password').fill(pw);
-  await page.getByRole('button', { name: 'next' }).click();
+  await page.locator('#submit-button').click();
   await page.context().storageState({ path: authFile });
 });
