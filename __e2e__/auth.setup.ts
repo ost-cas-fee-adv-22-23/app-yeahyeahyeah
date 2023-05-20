63d2ab1789ede3b321806410d4157b070821fa18
webrooster <roland.vonaesch@fhnw.ch>
2023-05-20 18:01:01 +0200

    config(Playwright): globalSetup

--- __e2e__/auth.setup.ts
+++ __e2e__/auth.setup.ts
@@ -1,24 +1,27 @@
-import { FullConfig, Page } from '@playwright/test';
+import { STORAGE_STATE } from './../playwright.config';
+import { expect, Browser, FullConfig, Page } from '@playwright/test';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 const user = process.env.ZITADEL_USER || '';
 const pw = process.env.ZITADEL_PW || '';
+const url = process.env.ZITADEL_ISSUER || '';
 
-const globalSetup = async (page: Page, config: FullConfig) => {
-  const authFile = './playwright/.auth/user.json';
+async function globalSetup(page: Page, config: FullConfig, browser: Browser) {
   const { baseURL } = config.projects[0].use;
 
   await Promise.all([
     page.goto(baseURL!),
     page.getByRole('button', { name: 'Login' }).click(),
+    expect(page).toHaveURL(url),
     page.getByPlaceholder('username@domain').click(),
     page.getByPlaceholder('username@domain').fill(user),
     page.locator('#submit-button').click(),
     page.getByLabel('Password').fill(pw),
     page.locator('#submit-button').click(),
-    page.context().storageState({ path: authFile }),
+    page.context().storageState({ path: STORAGE_STATE }),
+    console.log(await page.context().storageState()),
   ]);
-};
+}
 
 export default globalSetup;
