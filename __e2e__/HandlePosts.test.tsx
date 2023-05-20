9e71368f5eecdf407c1afe84dd79ffcbe93ebe9e
webrooster <roland.vonaesch@fhnw.ch>
2023-05-20 20:15:42 +0200

    feat(Test): post and delete message

--- __e2e__/HandlePosts.test.tsx
+++ __e2e__/HandlePosts.test.tsx
@@ -1,36 +1,47 @@
 import { test, expect } from '@playwright/test';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-const testMessage = 'Lorem ipsum dolor ...';
+test.describe('handle messages', () => {
+  const testMessage = 'Lorem ipsum dolor ...';
 
-test('A - timeline post - should post a mumble message', async ({ page }) => {
-  await Promise.all([
-    page.goto('/'),
-    page.waitForSelector('[data-testid="testTextarea"]'),
-    page.getByTestId('testTextarea').fill(testMessage),
-    page.waitForTimeout(3000),
-    page.getByRole('button', { name: 'Absenden' }).click(),
-  ]);
+  test('01.timeline - post & delete message', async ({ page }) => {
+    await Promise.all([
+      page.goto('/'),
+      page.waitForSelector('[data-testid="testTextarea"]'),
+      page.getByTestId('testTextarea').fill(testMessage),
+      page.waitForTimeout(3000),
+      page.getByRole('button', { name: 'Absenden' }).click(),
+      expect(page.getByRole('article').filter({ hasText: `${testMessage}` })).toBeInViewport,
+    ]);
+
+    // DELETE POSTED MESSAGE
+    const article = page.getByRole('article').filter({ hasText: `${testMessage}` });
+    const articleId = await article.getAttribute('id');
+    const svgElement = article.locator('svg').last();
+
+    await Promise.all([
+      page.waitForTimeout(3000),
+      svgElement.click(),
+      page.waitForTimeout(3000),
+      expect(page.locator(`body:has(#${articleId})`)).not.toContain,
+    ]);
+  });
+
+  test('02.timeline - textbox without message text', async ({ page }) => {
+    await Promise.all([
+      page.goto('/'),
+      page.waitForTimeout(3000),
+      page.getByRole('button', { name: 'Absenden' }).click(),
+      expect(page.getByText('Das Textfeld darf nicht leer sein.', { exact: true })).toBeVisible(),
+    ]);
+  });
 });
 
-test('B - timeline post error - should render an error', async ({ page }) => {
-  await Promise.all([
-    page.goto('/'),
-    page.waitForTimeout(3000),
-    page.getByRole('button', { name: 'Absenden' }).click(),
-    expect(page.getByText('Das Textfeld darf nicht leer sein.', { exact: true })).toBeVisible(),
-  ]);
-});
-
-test('C - timeline find post - mumble exists on timeline', async ({ page }) => {
-  await page.goto('/');
-  await page.waitForTimeout(3000);
-  await page.getByRole('article').filter({ hasText: `${testMessage}` });
-});
-
-test('D - profile find and delete', async ({ page }) => {
-  await page.goto('/profile/201161756305785089');
-  await page.reload();
-  const article = await page.getByRole('article').filter({ hasText: `${testMessage}` });
-  const svgElement = await article.locator('svg').last();
-  await svgElement.click();
-});
+// test('D - profile find and delete', async ({ page }) => {
+//   await page.goto('/profile/201161756305785089');
+//   await page.reload();
+//   const article = await page.getByRole('article').filter({ hasText: `${testMessage}` });
+//   const svgElement = await article.locator('svg').last();
+//   await svgElement.click();
+// });
