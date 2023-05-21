d0d4c681fdb25e001a29e9ede6cb58c714e858a2
webrooster <roland.vonaesch@fhnw.ch>
2023-05-21 23:17:35 +0200

    feat(Tests): better assertions

--- __e2e__/HandlePosts.test.tsx
+++ __e2e__/HandlePosts.test.tsx
@@ -3,6 +3,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 test.describe.configure({ mode: 'serial' });
+
 const testMessage = 'Lorem ipsum dolor ...';
 test.describe('handle messages', () => {
   test('01.timeline - post message', async ({ page }) => {
@@ -18,20 +19,24 @@ test.describe('handle messages', () => {
 
   test('02.timeline - delete message', async ({ page }) => {
     await page.goto('/');
+    await page.waitForTimeout(3000);
 
     const hasArticleToBeDelete = await page.isVisible(`text=${testMessage}`);
-    console.log(hasArticleToBeDelete === true ? '✔️ test message is present' : '❗ no test message is present');
+    console.log(hasArticleToBeDelete === true ? '✔️ test message is present' : '❗ no test message found');
 
-    if (hasArticleToBeDelete) {
-      console.log('✔️ Test article found in stream. Try to delete this article.');
-      const articleToBeDeleted = await page.getByRole('article').filter({ hasText: `${testMessage}` });
+    if (hasArticleToBeDelete === true) {
+      const articleToBeDeleted = page.getByRole('article').filter({ hasText: `${testMessage}` });
+      expect(articleToBeDeleted, '✔️ test article found in stream. Try to delete this article.').toBeTruthy;
+
       const article_id = await articleToBeDeleted.getAttribute('id');
-      expect(page.locator(`body:has(#${article_id})`)).not.toBeVisible;
       await articleToBeDeleted.locator('svg').last().click();
+      expect(page.locator(`body:has(#${article_id})`)).not.toBeVisible;
     }
 
-    expect(await page.getByRole('article').filter({ hasText: `${testMessage}` })).not.toBeVisible;
-    console.log('✔️ no test message is present. All messages are deleted.');
+    expect(
+      page.getByRole('article').filter({ hasText: `${testMessage}` }),
+      '❌ something went wrong. there are still test messages present'
+    ).not.toBeInViewport;
   });
 
   test('03.timeline - post without text', async ({ page }) => {
