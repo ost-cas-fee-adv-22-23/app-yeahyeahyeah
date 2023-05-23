2af67fcc9e417f21fff91ceeef77ab8b3fa53d68
webrooster <roland.vonaesch@fhnw.ch>
2023-05-23 09:42:23 +0200

    feat(Playwright): first test non authenticated

--- __e2e__/non-authenticated.test.tsx
+++ __e2e__/non-authenticated.test.tsx
@@ -8,15 +8,18 @@ test.beforeEach(async ({ page, context }) => {
   await page.waitForSelector('body');
   await expect(page.getByLabel('Login')).toBeInViewport();
   await expect(page.getByLabel('Logout')).not.toBeInViewport();
+  // console.log(await context.storageState());
 });
 
-test.describe('02.non authenticated', () => {
-  test('02.timeline - post without text', async ({ page }) => {
-    await Promise.all([
-      page.goto('/'),
-      page.waitForSelector('body'),
-      page.getByRole('button', { name: 'Absenden' }).click(),
-      expect(page.getByText('Das Textfeld darf nicht leer sein.', { exact: true })).toBeVisible,
-    ]);
+test.describe('01.non-authenticated', () => {
+  test('01.timeline - should redirect ', async ({ page }) => {
+    await page.goto('/');
+    await page.waitForSelector('body');
+    const article = await page.getByRole('article').filter({ hasText: 'username' }).first();
+    await article.locator('a').first().click();
+    await page.waitForSelector('body');
+    await expect(page).toHaveURL(/landing/);
+    await expect(page.locator('h1')).toContainText('Find out whats new');
+    await expect(page).toHaveTitle(`Mumble - Willkommen auf der Mumble App des CAS Frontend Engineer's Advanced`);
   });
 });
