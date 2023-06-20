57eb2f71f10a8b080d7aa4888764e308359ca39d
Thomas Schallert <thomas.schallert@fhnw.ch>
2023-06-20 11:10:35 +0200

    feat: run healthcheck after deployment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ export default function Page({
   fallbackUsers: QwackerUserResponse;
   fallbackUserLoggedIn: User;
 }) {
-  console.log(`Testing workflows :-)`);
+  console.log(`Testing workflows 1 :-)`);
   return (
     <>
       <NextSeo title={`${Content.seo.home.title}`} description={`${Content.seo.home.description}`} />
