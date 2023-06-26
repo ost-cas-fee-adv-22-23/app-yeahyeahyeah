812e5041053f90ddc97fa9a6e2eba08ad5d9d0c6
Thomas Schallert <thomas.schallert@fhnw.ch>
2023-06-26 16:51:15 +0200

    fix: twin tests

--- __tests__/twin/Test.test.tsx
+++ __tests__/twin/Test.test.tsx
@@ -3,8 +3,8 @@ import Test from './Test';
 
 describe('Testing twin and styled components', () => {
   it('finds a predefined sentence', () => {
-    const { container } = render(<Test text="i like styled components" />);
-    const text = screen.getByText(/i like styled components/i);
+    render(<Test text="i like styled components" />);
+    const text = screen.getByText(/i like styled components/);
     expect(text).toHaveTextContent('i like styled components');
   });
 
@@ -18,5 +18,18 @@ describe('Testing twin and styled components', () => {
     const { container } = render(<Test text="i like styled components" />);
     const div = container.getElementsByTagName('div');
     expect(div.length).toEqual(2);
+  });
+
+  it('finds specific classnames that get rendered with twin and styled components', () => {
+    const { container } = render(<Test text="i like styled components" />);
+
+    const StyledImportComponent = container.querySelector('[class*=Test__StyledImportComponent]');
+    expect(StyledImportComponent).toBeInTheDocument();
+
+    const TwImportComponent = container.querySelector('[class*=Test__TwImportComponent]');
+    expect(TwImportComponent).toBeInTheDocument();
+
+    const StyledDiv = container.querySelector('[class*=Test___StyledDiv]');
+    expect(StyledDiv).toBeInTheDocument();
   });
 });
